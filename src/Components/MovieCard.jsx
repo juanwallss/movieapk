cf7efb1354b301ad1b453c6c8f1e6dcc5d33987c
Juan Carlos Paredes <92334667+juanwallss@users.noreply.github.com>
2022-07-23 11:46:55 -0700

    modified styles

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,16 +1,44 @@
-import styles from "./MovieCard.module.css";
-import placeHolder from "../no-image.jpg";
-import { Link } from "react-router-dom";
-export function MovieCard({movie}){
-    const imageUrl = movie.poster_path ? "https://image.tmdb.org/t/p/w300" + movie.poster_path : placeHolder;
-    return  <li className={styles.movieCard}>
-    <Link to={"/movies/" + movie.id}>
-    <img width={230}
-    height={345}
-    src={imageUrl} 
-    className={styles.movieImage} 
-    alt={movie.title} />
-    </Link>
-    {movie.title}</li>
-}
+import styles from './MovieCard.module.css'
+import placeHolder from '../no-image.jpg'
+import { Link } from 'react-router-dom'
+export function MovieCard({ movie }) {
+	const imageUrl = movie.poster_path
+		? 'https://image.tmdb.org/t/p/w300' + movie.poster_path
+		: placeHolder
 
+	return (
+		<li className={styles.movieCard}>
+			<Link to={'/movies/' + movie.id}>
+				<img
+					width={230}
+					height={345}
+					src={imageUrl}
+					className={styles.movieImage}
+					alt={movie.title}
+				/>
+			</Link>
+			{movie.title}
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+				}}
+			>
+				<div
+					style={{
+						backgroundColor: '#335c3e',
+						width: '30%',
+						borderRadius: '15px',
+						display: 'flex',
+						justifyContent: 'center',
+						alignContent: 'center',
+						alignItems: 'center',
+						justifyItems: 'center',
+					}}
+				>
+					{movie.release_date.slice(0, 4)}
+				</div>
+			</div>
+		</li>
+	)
+}
--- src/Components/MoviesGrid.jsx
+++ src/Components/MoviesGrid.jsx
@@ -1,58 +1,52 @@
-import movies from "./movies.json";
-import styles from "./MoviesGrid.module.css";
-import {MovieCard} from "./MovieCard";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { get } from "../utils/httpClients";
-import { Spinner } from "./Spinner";
-import { useLocation } from 'react-router';
-import  {useQuery} from "../hooks/useQuery";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { Empty } from "./Empty";
+import movies from './movies.json'
+import styles from './MoviesGrid.module.css'
+import { MovieCard } from './MovieCard'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { get } from '../utils/httpClients'
+import { Spinner } from './Spinner'
+import { useLocation } from 'react-router'
+import { useQuery } from '../hooks/useQuery'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { Empty } from './Empty'
 
+function MoviesGrid({ search }) {
+	const [isLoading, setIsLoading] = useState(true)
+	const [movies, setMovies] = useState([])
+	const [page, setPage] = useState(1)
+	const [hasMore, setHasMore] = useState(true)
 
-function MoviesGrid({search}){
-    const [isLoading, setIsLoading] = useState(true);
-    const [movies, setMovies] = useState([]);  
-    const [page, setPage] = useState(1);
-    const [hasMore, setHasMore] = useState(true);
+	useEffect(() => {
+		setIsLoading(true)
+		const searchUrl = search
+			? '/search/movie?query=' + search + '&page=' + page
+			: '/discover/movie?page=' + page
+		get(searchUrl).then((data) => {
+			setMovies((prevMovies) => prevMovies.concat(data.results))
+			setHasMore(data.page < data.total_pages)
+			console.log(movies)
+			setIsLoading(false)
+		})
+	}, [search, page])
 
-    
+	if (!isLoading && movies.length === 0) {
+		return <Empty />
+	}
 
-    useEffect(() => {
-        setIsLoading(true);
-        const searchUrl = search 
-        ? "/search/movie?query=" + search +"&page="+page
-        : "/discover/movie?page=" +page;
-        get(searchUrl)
-          .then((data) => {
-              setIsLoading(false);
-            setMovies(prevMovies => prevMovies.concat(data.results));           
-            setHasMore(data.page < data.total_pages);
-        });
-    }, [search, page]);
-   
-    if(!isLoading && movies.length === 0){
-        return <Empty />
-    }
-
-    return (
-        <InfiniteScroll 
-        dataLength={movies.length}
-        hasMore={hasMore}
-        next={() => setPage((prevPage) => prevPage +1 )}
-        loader={<Spinner />} >
-        <ul className={styles.moviesGrid}>
-            {movies.map((movie) => 
-               <MovieCard 
-               key={movie.id} 
-               movie={movie}>       
-               </MovieCard>
-            )}
-        </ul>
-        </InfiniteScroll>
-    )
+	return (
+		<InfiniteScroll
+			dataLength={movies.length}
+			hasMore={hasMore}
+			next={() => setPage((prevPage) => prevPage + 1)}
+			loader={<Spinner />}
+		>
+			<ul className={styles.moviesGrid}>
+				{movies.map((movie) => (
+					<MovieCard key={movie.id} movie={movie}></MovieCard>
+				))}
+			</ul>
+		</InfiniteScroll>
+	)
 }
 
-
-export default MoviesGrid;
+export default MoviesGrid
